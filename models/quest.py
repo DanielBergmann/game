a873c7640e9df45b0708af8a5b493af114d9a7d7
Daniel Lotsman <ddaannttee1155@gmail.com>
2023-11-06 21:53:16 +0200

    remove trash

--- models/quest.py
+++
@@ -1,5 +0,0 @@
-class Quest:
-    def __init__(self, name: str, description: str, reward: float = 0.0):
-        self.name = name
-        self.description = description
-        self.reward = reward
--- models/utils.py
+++
@@ -1,5 +0,0 @@
-def calculate_weight(things: list):
-    total_weight = 0.0
-    for thing in things:
-        total_weight += thing.weight
-    return total_weight
